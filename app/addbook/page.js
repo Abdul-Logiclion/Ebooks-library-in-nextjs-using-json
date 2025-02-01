2cf1b180f246731cae22efd0f5a4ab06caca2c35
Abdul-Logiclion <abduljabarsc01@gmail.com>
2025-02-01 12:51:07 +0500

    old repo was in html now replaced same code in nextjs as a whole frontend and backend both

--- app/addbook/page.js
+++ app/addbook/page.js
@@ -1,12 +1,11 @@
-
 'use client'
+import { Suspense } from 'react';
 import BookEditor from "../components/BookEditor";
 
-export default function AddBookPage()
-{
-
-    return <>
-    <BookEditor/>
-    </>
+export default function AddBookPage() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <BookEditor />
+        </Suspense>
+    );
 }
-
